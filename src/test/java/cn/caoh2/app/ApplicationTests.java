54918113906f0959c8029683b4b7375fdd20eb42
cao0109 <102588999+cao0109@users.noreply.github.com>
2023-03-16 09:20:17 +0800

    初始化项目

---
+++ src/test/java/cn/caoh2/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.caoh2.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
