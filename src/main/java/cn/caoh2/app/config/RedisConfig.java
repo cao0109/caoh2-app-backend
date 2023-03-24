65f92e3b03cc4bea41029f70310fe747a049549a
cao0109 <102588999+cao0109@users.noreply.github.com>
2023-03-24 14:22:22 +0800

    修改 redis 序列化

--- src/main/java/cn/caoh2/app/config/RedisConfig.java
+++ src/main/java/cn/caoh2/app/config/RedisConfig.java
@@ -10,33 +10,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 /**
  * @Author caoh2
  * @Date 2023/3/18 14:28
- * @Description: redis配置 主要是配置Redis的序列化规则，用Jackson2JsonRedisSerializer替换默认的jdkSerializer
+ * @Description: redis配置
  * @Version 1.0
  */
 @Configuration
 @SuppressWarnings("all")
 public class RedisConfig {
 
-
     @Bean
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-
-        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        FastJsonRedisSerializer<Object> serializer = new FastJsonRedisSerializer<>(Object.class);
-//        ObjectMapper mapper = new ObjectMapper();
-//        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        serializer.setObjectMapper(mapper);
-
-        // 设置value的序列化规则和 key的序列化规则
-        redisTemplate.setValueSerializer(serializer);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(serializer);
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        // 使用fastjson序列化
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        // value值的序列化采用fastJsonRedisSerializer
+        template.setValueSerializer(fastJsonRedisSerializer);
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+        // key的序列化采用StringRedisSerializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
     }
 }
--- src/main/java/cn/caoh2/app/utils/FastJsonRedisSerializer.java
+++ src/main/java/cn/caoh2/app/utils/FastJsonRedisSerializer.java
@@ -3,8 +3,6 @@ package cn.caoh2.app.utils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
@@ -13,53 +11,43 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * @Author caoh2
- * @Date 2023/3/18 22:12
- * @Description: Redis使用FastJson序列化
+ * @Date 2023/3/24 13:44
+ * @Description: fastjson序列化
  * @Version 1.0
  */
 
+
 public class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
 
-    public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
-
-    private final Class<T> clazz;
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
     static {
-        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        ParserConfig.getGlobalInstance().addAccept("cn.caoh2.app.entity.JwtUser");
+        ParserConfig.getGlobalInstance().addAccept("org.springframework.security.core.authority.SimpleGrantedAuthority");
     }
 
+    private final Class<T> clazz;
 
     public FastJsonRedisSerializer(Class<T> clazz) {
         super();
         this.clazz = clazz;
     }
 
-    /*
-     * 序列化
-     */
     @Override
     public byte[] serialize(T t) throws SerializationException {
-        if (t == null) {
+        if (null == t) {
             return new byte[0];
         }
         return JSON.toJSONString(t, SerializerFeature.WriteClassName).getBytes(DEFAULT_CHARSET);
     }
 
-
-    /*
-     * 反序列化
-     */
     @Override
     public T deserialize(byte[] bytes) throws SerializationException {
-        if (bytes == null || bytes.length == 0) {
+        if (null == bytes || bytes.length == 0) {
             return null;
         }
         String str = new String(bytes, DEFAULT_CHARSET);
-
-        return JSON.parseObject(str, clazz);
-    }
-
-    protected JavaType getJavaType(Class<?> clazz) {
-        return TypeFactory.defaultInstance().constructType(clazz);
+        return (T) JSON.parseObject(str, clazz);
     }
 }
+
